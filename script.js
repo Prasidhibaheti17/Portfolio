9b9e89cfbee9ea7fe9ac35f1f5cc23225cd57bf5
Anish Agrawal <114282241+pacd321@users.noreply.github.com>
2023-10-30 22:58:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
-let menuIcon=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
-
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-
-
-let sections=document.querySelectorAll('section');
-let navLinks=document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-
+---
+---
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('.navbar');
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('header nav a');
+    const header = document.querySelector('header');
+    
+    // Add a click event listener to the menu icon
+    menuIcon.addEventListener('click', function() {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
     });
-    let header=document.querySelector('header');
+    
+    // Add a scroll event listener
+    window.addEventListener('scroll', function() {
+        const top = window.scrollY;
+        navLinks.forEach(link => link.classList.remove('active'));
 
-    header.classList.toggle('sticky',window.screenY>100);
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-};
+        sections.forEach(section => {
+            const offset = section.offsetTop - 150;
+            const height = section.offsetHeight;
+            const id = section.getAttribute('id');
+            
+            if (top >= offset && top < offset + height) {
+                const matchingLink = document.querySelector(`header nav a[href*=${id}]`);
+                if (matchingLink) {
+                    matchingLink.classList.add('active');
+                }
+            }
+        });
 
-ScrollReveal({
-    //reset:true,
-    distance:'80px',
-    duration:2000,
-    delay:200
-});
-
-ScrollReveal().reveal('.home-content,.heading',{origin:'top'});
-ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form',{origin:'bottom'});
-ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
-ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
-
-const typed = new Typed('.multiple-text',{
-    strings:['CSE Student at SRM UNIVERSITY ','Frontend developer'],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
+        // Toggle the 'sticky' class on the header
+        header.classList.toggle('sticky', top > 100);
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+    });
+    
+    // Initialize ScrollReveal
+    ScrollReveal({
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+    
+    // Define ScrollReveal animations
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+    
+    // Initialize Typed.js
+    const typed = new Typed('.multiple-text', {
+        strings: ['CSE Student at SRM UNIVERSITY', 'Frontend developer'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
 });
